aac591fcba30dc3ffd63bafa30fab46d6ab9a16f
RosChicken <97747413+TeniRos@users.noreply.github.com>
2023-11-09 14:42:20 +0900

    更新 custom.ts

--- custom.ts
+++ custom.ts
@@ -16,16 +16,15 @@ namespace LedPlugin {
     //% block="1列に2進数で %num の値を %col の列から表示する"
     //% inlineInputMode=inline
     export function DisplayBit5(num: number, col: number) {
-
-        let hoge = num
+        
         //一旦Ledの消去
         for (let i = 0; i < 5; i++) { led.unplot(i, col) }
         //どんどん厳選
-        if (16 <= hoge) { led.plot(4, col); hoge -= 16 }
-        if (8 <= hoge) { led.plot(3, col); hoge -= 8 }
-        if (4 <= hoge) { led.plot(2, col); hoge -= 4 }
-        if (2 <= hoge) { led.plot(1, col); hoge -= 2 }
-        if (1 <= hoge) { led.plot(0, col); hoge -= 1 }
+        if (16 <= num) { led.plot(4, col); num -= 16 }
+        if (8 <= num) { led.plot(3, col); num -= 8 }
+        if (4 <= num) { led.plot(2, col); num -= 4 }
+        if (2 <= num) { led.plot(1, col); num -= 2 }
+        if (1 <= num) { led.plot(0, col); num -= 1 }
     }
 
     //2列
@@ -37,22 +36,22 @@ namespace LedPlugin {
     //% block="2列に2進数で %num の値を %col の列から表示する"
     //% inlineInputMode=inline
     export function DisplayBit10(num: number, col: number) {
-        let hoge = num
+        
         //一旦Ledの消去
         for (let i = 0; i < 5; i++) { led.unplot(i, col); led.unplot(i, col + 1); }
         //どんどん厳選
         //2列
-        if (512 <= hoge) { led.plot(4, col + 1); hoge -= 512 }
-        if (256 <= hoge) { led.plot(3, col + 1); hoge -= 256 }
-        if (128 <= hoge) { led.plot(2, col + 1); hoge -= 128 }
-        if (64 <= hoge) { led.plot(1, col + 1); hoge -= 64 }
-        if (32 <= hoge) { led.plot(0, col + 1); hoge -= 32 }
+        if (512 <= num) { led.plot(4, col + 1); num -= 512 }
+        if (256 <= num) { led.plot(3, col + 1); num -= 256 }
+        if (128 <= num) { led.plot(2, col + 1); num -= 128 }
+        if (64 <= num) { led.plot(1, col + 1); num -= 64 }
+        if (32 <= num) { led.plot(0, col + 1); num -= 32 }
         //1列
-        if (16 <= hoge) { led.plot(4, col); hoge -= 16 }
-        if (8 <= hoge) { led.plot(3, col); hoge -= 8 }
-        if (4 <= hoge) { led.plot(2, col); hoge -= 4 }
-        if (2 <= hoge) { led.plot(1, col); hoge -= 2 }
-        if (1 <= hoge) { led.plot(0, col); hoge -= 1 }
+        if (16 <= num) { led.plot(4, col); num -= 16 }
+        if (8 <= num) { led.plot(3, col); num -= 8 }
+        if (4 <= num) { led.plot(2, col); num -= 4 }
+        if (2 <= num) { led.plot(1, col); num -= 2 }
+        if (1 <= num) { led.plot(0, col); num -= 1 }
     }
 
     //3列
@@ -64,28 +63,28 @@ namespace LedPlugin {
     //% block="3列に2進数で %num の値を %col の列から表示する"
     //% inlineInputMode=inline
     export function DisplayBit15(num: number, col: number) {
-        let hoge = num
+        
         //一旦Ledの消去
         for (let i = 0; i < 5; i++) { led.unplot(i, col); led.unplot(i, col + 1); led.unplot(i, col + 2); }
         //どんどん厳選
         //3列
-        if (16384 <= hoge) { led.plot(4, col + 2); hoge -= 16384 }
-        if (8192 <= hoge) { led.plot(3, col + 2); hoge -= 8192 }
-        if (4096 <= hoge) { led.plot(2, col + 2); hoge -= 4096 }
-        if (2048 <= hoge) { led.plot(1, col + 2); hoge -= 2048 }
-        if (1024 <= hoge) { led.plot(0, col + 2); hoge -= 1024 }
+        if (16384 <= num) { led.plot(4, col + 2); num -= 16384 }
+        if (8192 <= num) { led.plot(3, col + 2); num -= 8192 }
+        if (4096 <= num) { led.plot(2, col + 2); num -= 4096 }
+        if (2048 <= num) { led.plot(1, col + 2); num -= 2048 }
+        if (1024 <= num) { led.plot(0, col + 2); num -= 1024 }
         //2列
-        if (512 <= hoge) { led.plot(4, col + 1); hoge -= 512 }
-        if (256 <= hoge) { led.plot(3, col + 1); hoge -= 256 }
-        if (128 <= hoge) { led.plot(2, col + 1); hoge -= 128 }
-        if (64 <= hoge) { led.plot(1, col + 1); hoge -= 64 }
-        if (32 <= hoge) { led.plot(0, col + 1); hoge -= 32 }
+        if (512 <= num) { led.plot(4, col + 1); num -= 512 }
+        if (256 <= num) { led.plot(3, col + 1); num -= 256 }
+        if (128 <= num) { led.plot(2, col + 1); num -= 128 }
+        if (64 <= num) { led.plot(1, col + 1); num -= 64 }
+        if (32 <= num) { led.plot(0, col + 1); num -= 32 }
         //1列
-        if (16 <= hoge) { led.plot(4, col); hoge -= 16 }
-        if (8 <= hoge) { led.plot(3, col); hoge -= 8 }
-        if (4 <= hoge) { led.plot(2, col); hoge -= 4 }
-        if (2 <= hoge) { led.plot(1, col); hoge -= 2 }
-        if (1 <= hoge) { led.plot(0, col); hoge -= 1 }
+        if (16 <= num) { led.plot(4, col); num -= 16 }
+        if (8 <= num) { led.plot(3, col); num -= 8 }
+        if (4 <= num) { led.plot(2, col); num -= 4 }
+        if (2 <= num) { led.plot(1, col); num -= 2 }
+        if (1 <= num) { led.plot(0, col); num -= 1 }
     }
 
     //4列
@@ -97,34 +96,34 @@ namespace LedPlugin {
     //% block="4列に2進数で %num の値を %col の列から表示する"
     //% inlineInputMode=inline
     export function DisplayBit20(num: number, col: number) {
-        let hoge = num
+        
         //一旦Ledの消去
         for (let i = 0; i < 5; i++) { led.unplot(i, col); led.unplot(i, col + 1); led.unplot(i, col + 2); led.unplot(i, col + 3); }
         //どんどん厳選
         //4列
-        if (524288 <= hoge) { led.plot(4, col + 3); hoge -= 524288 }
-        if (262144 <= hoge) { led.plot(3, col + 3); hoge -= 262144 }
-        if (131072 <= hoge) { led.plot(2, col + 3); hoge -= 131072 }
-        if (65536 <= hoge) { led.plot(1, col + 3); hoge -= 65536 }
-        if (32768 <= hoge) { led.plot(0, col + 3); hoge -= 32768 }
+        if (524288 <= num) { led.plot(4, col + 3); num -= 524288 }
+        if (262144 <= num) { led.plot(3, col + 3); num -= 262144 }
+        if (131072 <= num) { led.plot(2, col + 3); num -= 131072 }
+        if (65536 <= num) { led.plot(1, col + 3); num -= 65536 }
+        if (32768 <= num) { led.plot(0, col + 3); num -= 32768 }
         //3列
-        if (16384 <= hoge) { led.plot(4, col + 2); hoge -= 16384 }
-        if (8192 <= hoge) { led.plot(3, col + 2); hoge -= 8192 }
-        if (4096 <= hoge) { led.plot(2, col + 2); hoge -= 4096 }
-        if (2048 <= hoge) { led.plot(1, col + 2); hoge -= 2048 }
-        if (1024 <= hoge) { led.plot(0, col + 2); hoge -= 1024 }
+        if (16384 <= num) { led.plot(4, col + 2); num -= 16384 }
+        if (8192 <= num) { led.plot(3, col + 2); num -= 8192 }
+        if (4096 <= num) { led.plot(2, col + 2); num -= 4096 }
+        if (2048 <= num) { led.plot(1, col + 2); num -= 2048 }
+        if (1024 <= num) { led.plot(0, col + 2); num -= 1024 }
         //2列
-        if (512 <= hoge) { led.plot(4, col + 1); hoge -= 512 }
-        if (256 <= hoge) { led.plot(3, col + 1); hoge -= 256 }
-        if (128 <= hoge) { led.plot(2, col + 1); hoge -= 128 }
-        if (64 <= hoge) { led.plot(1, col + 1); hoge -= 64 }
-        if (32 <= hoge) { led.plot(0, col + 1); hoge -= 32 }
+        if (512 <= num) { led.plot(4, col + 1); num -= 512 }
+        if (256 <= num) { led.plot(3, col + 1); num -= 256 }
+        if (128 <= num) { led.plot(2, col + 1); num -= 128 }
+        if (64 <= num) { led.plot(1, col + 1); num -= 64 }
+        if (32 <= num) { led.plot(0, col + 1); num -= 32 }
         //1列
-        if (16 <= hoge) { led.plot(4, col); hoge -= 16 }
-        if (8 <= hoge) { led.plot(3, col); hoge -= 8 }
-        if (4 <= hoge) { led.plot(2, col); hoge -= 4 }
-        if (2 <= hoge) { led.plot(1, col); hoge -= 2 }
-        if (1 <= hoge) { led.plot(0, col); hoge -= 1 }
+        if (16 <= num) { led.plot(4, col); num -= 16 }
+        if (8 <= num) { led.plot(3, col); num -= 8 }
+        if (4 <= num) { led.plot(2, col); num -= 4 }
+        if (2 <= num) { led.plot(1, col); num -= 2 }
+        if (1 <= num) { led.plot(0, col); num -= 1 }
     }
 
     //5列
@@ -136,39 +135,39 @@ namespace LedPlugin {
     //% block="5列に2進数で %num の値を %col の列から表示する"
     //% inlineInputMode=inline
     export function DisplayBit25(num: number, col: number) {
-        let hoge = num
+        
         //一旦Ledの消去
         for (let i = 0; i < 5; i++) { led.unplot(i, col); led.unplot(i, col + 1); led.unplot(i, col + 2); led.unplot(i, col + 3); led.unplot(i, col + 4); }
         //どんどん厳選
         //5列
-        if (16777216 <= hoge) { led.plot(4, col + 4); hoge -= 16777216 }
-        if (8388608 <= hoge) { led.plot(3, col + 4); hoge -= 8388608 }
-        if (4194304 <= hoge) { led.plot(2, col + 4); hoge -= 4194304 }
-        if (2097152 <= hoge) { led.plot(1, col + 4); hoge -= 2097152 }
-        if (1048576 <= hoge) { led.plot(0, col + 4); hoge -= 1048576 }
+        if (16777216 <= num) { led.plot(4, col + 4); num -= 16777216 }
+        if (8388608 <= num) { led.plot(3, col + 4); num -= 8388608 }
+        if (4194304 <= num) { led.plot(2, col + 4); num -= 4194304 }
+        if (2097152 <= num) { led.plot(1, col + 4); num -= 2097152 }
+        if (1048576 <= num) { led.plot(0, col + 4); num -= 1048576 }
         //4列
-        if (524288 <= hoge) { led.plot(4, col + 3); hoge -= 524288 }
-        if (262144 <= hoge) { led.plot(3, col + 3); hoge -= 262144 }
-        if (131072 <= hoge) { led.plot(2, col + 3); hoge -= 131072 }
-        if (65536 <= hoge) { led.plot(1, col + 3); hoge -= 65536 }
-        if (32768 <= hoge) { led.plot(0, col + 3); hoge -= 32768 }
+        if (524288 <= num) { led.plot(4, col + 3); num -= 524288 }
+        if (262144 <= num) { led.plot(3, col + 3); num -= 262144 }
+        if (131072 <= num) { led.plot(2, col + 3); num -= 131072 }
+        if (65536 <= num) { led.plot(1, col + 3); num -= 65536 }
+        if (32768 <= num) { led.plot(0, col + 3); num -= 32768 }
         //3列
-        if (16384 <= hoge) { led.plot(4, col + 2); hoge -= 16384 }
-        if (8192 <= hoge) { led.plot(3, col + 2); hoge -= 8192 }
-        if (4096 <= hoge) { led.plot(2, col + 2); hoge -= 4096 }
-        if (2048 <= hoge) { led.plot(1, col + 2); hoge -= 2048 }
-        if (1024 <= hoge) { led.plot(0, col + 2); hoge -= 1024 }
+        if (16384 <= num) { led.plot(4, col + 2); num -= 16384 }
+        if (8192 <= num) { led.plot(3, col + 2); num -= 8192 }
+        if (4096 <= num) { led.plot(2, col + 2); num -= 4096 }
+        if (2048 <= num) { led.plot(1, col + 2); num -= 2048 }
+        if (1024 <= num) { led.plot(0, col + 2); num -= 1024 }
         //2列
-        if (512 <= hoge) { led.plot(4, col + 1); hoge -= 512 }
-        if (256 <= hoge) { led.plot(3, col + 1); hoge -= 256 }
-        if (128 <= hoge) { led.plot(2, col + 1); hoge -= 128 }
-        if (64 <= hoge) { led.plot(1, col + 1); hoge -= 64 }
-        if (32 <= hoge) { led.plot(0, col + 1); hoge -= 32 }
+        if (512 <= num) { led.plot(4, col + 1); num -= 512 }
+        if (256 <= num) { led.plot(3, col + 1); num -= 256 }
+        if (128 <= num) { led.plot(2, col + 1); num -= 128 }
+        if (64 <= num) { led.plot(1, col + 1); num -= 64 }
+        if (32 <= num) { led.plot(0, col + 1); num -= 32 }
         //1列
-        if (16 <= hoge) { led.plot(4, col); hoge -= 16 }
-        if (8 <= hoge) { led.plot(3, col); hoge -= 8 }
-        if (4 <= hoge) { led.plot(2, col); hoge -= 4 }
-        if (2 <= hoge) { led.plot(1, col); hoge -= 2 }
-        if (1 <= hoge) { led.plot(0, col); hoge -= 1 }
+        if (16 <= num) { led.plot(4, col); num -= 16 }
+        if (8 <= num) { led.plot(3, col); num -= 8 }
+        if (4 <= num) { led.plot(2, col); num -= 4 }
+        if (2 <= num) { led.plot(1, col); num -= 2 }
+        if (1 <= num) { led.plot(0, col); num -= 1 }
     }
 }
